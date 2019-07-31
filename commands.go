2c2e99386ec65151456af56c5e614ba2a105a58d
fiatjaf <fiatjaf@gmail.com>
2019-07-31 02:00:16 +0000

    apps default to some meaningful thing instead of help. /help_appname should work for all.

--- commands.go
+++ commands.go
@@ -84,19 +84,19 @@ var methods = []def{
 	},
 	def{
 		aliases: []string{"microbet", "app microbet"},
-		argstr:  "[bet | bets | balance | withdraw]",
+		argstr:  "[bets | balance | withdraw | bet]",
 	},
 	def{
 		aliases: []string{"bitflash", "app bitflash"},
-		argstr:  "[orders | status | rate | <satoshis> <address>]",
+		argstr:  "(orders | status | rate | <satoshis> <address>)",
 	},
 	def{
 		aliases: []string{"satellite", "app satellite"},
-		argstr:  "[transmissions | queue | bump <satoshis> <transmission_id> | delete <transmission_id> | <satoshis> <message>...]",
+		argstr:  "(transmissions | queue | bump <satoshis> <transmission_id> | delete <transmission_id> | <satoshis> <message>...)",
 	},
 	def{
 		aliases: []string{"golightning", "app golightning"},
-		argstr:  "[<satoshis>]",
+		argstr:  "<satoshis>",
 	},
 	def{
 		aliases: []string{"gifts", "app gifts"},
