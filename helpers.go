3a748679d027a641ec1400687b82628132f927c3
fiatjaf <fiatjaf@alhur.es>
2020-11-22 15:46:52 -0300

    disallow <1sat amounts

--- helpers.go
+++ helpers.go
@@ -54,13 +54,15 @@ func parseSatoshis(opts docopt.Opts) (msats int64, err error) {
 }
 
 func parseAmountString(amt string) (msats int64, err error) {
+	defer func() {
+		if msats < 1000 {
+			err = errors.New("too small")
+		}
+	}()
+
 	// is a number
 	sats, err := strconv.ParseFloat(amt, 64)
 	if err == nil {
-		if sats < 1 {
-			return 0, errors.New("too small")
-		}
-
 		return int64(sats * 1000), nil
 	}
 
